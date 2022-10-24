ec325b11d5bcea1a100c7ae8fd87718424aa4f26
Kevin martinez <108833304+KevinMartinezRocket@users.noreply.github.com>
2022-10-24 18:47:27 -0500

    add query

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -39,6 +39,6 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     res
       .status(200)
       .send(
-        `Hello! Your username2 is ${user.displayName} and the current channel is ${channel.displayName}.`
+        `Hello! Your username3 is ${user.displayName} and the current channel is ${channel.displayName}. anmd you send $(query) `
       );
   }
