2451740cd1df6c0c3c5c106430d3099b12cf1472
KevinMartinezRocket <kevin.martinez@therocketcode.com>
2023-01-28 00:31:23 -0600

    quit lock

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,6 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <video controls autoPlay  src="./multimedia/Moneda_control_1.webm" type='video/webm'>
-      <source src="./multimedia/Moneda_control_1.webm" type='video/webm'/>
-    </video>
   </Layout>
 )
 
