d94a00ba302b3ab53c19a941d454fc7e1a0d7cb3
KevinMartinezRocket <kevin.martinez@therocketcode.com>
2023-01-28 00:25:31 -0600

    add videos moneda

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,9 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <video controls autoPlay  src="./multimedia/Moneda_control_1.webm" type='video/webm'>
+      <source src="./multimedia/Moneda_control_1.webm" type='video/webm'/>
+    </video>
   </Layout>
 )
 
